ecbd8cd186eb0b3d799c2f215ef839008e6385bf
Takwa-Ayari <takwaayari1999@gmail.com>
2024-01-23 01:22:44 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl text-red-800 font-bold underline hover:text-blue-300">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
