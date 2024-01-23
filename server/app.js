eb06dc586b31d72f64feb834edc5bc030dcfa0c4
Takwa-Ayari <takwaayari1999@gmail.com>
2024-01-23 02:01:17 +0100

    connected to database

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const connection = require('./databaseConnection');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,8 +14,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+
+app.on('close', () => {
+    connection.end((err) => {
+        if (err) {
+            console.error('Error closing MySQL database connection: ' + err.stack);
+            return;
+        }
+        console.log('MySQL database connection closed.');
+    });
+});
 
 module.exports = app;
